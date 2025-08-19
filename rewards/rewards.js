2c1046fa51d9102921bab6fb38c9f40100100b15
Kostya Zinoview <k.gig@list.ru>
2025-08-19 23:02:36 +0300

    another try to init web app

--- rewards/rewards.js
+++ rewards/rewards.js
@@ -57,6 +57,28 @@ function countDownTimer(timerElement) {
 }
 
 window.onload = function () {
+  const telegramWA =  window.Telegram && window.Telegram.WebApp;
+  console.log(`Telegram webAppVersion telegramWA=${telegramWA}`)
+  if (telegramWA) {
+    telegramWA.ready();
+    console.log(`Telegram webAppVersion DOUBLE CHECK telegramWA=${telegramWA}`)
+
+    telegramWA.BackButton.show();
+    telegramWA.BackButton.onClick(() => {
+      alert("Back button pressed");
+      telegramWA.BackButton.hide(); // optional
+    });
+    
+    console.log(`window.Telegram.WebView=${window.Telegram.WebView}`)
+    console.log(`window.Telegram.WebView.initParams=${window.Telegram.WebView.initParams}`)
+    console.log(`Telegram webAppVersion telegramWA.initData=${telegramWA.initData}`)
+    console.log(`Telegram webAppVersion telegramWA.initDataUnsafe=${telegramWA.initDataUnsafe}`)
+    console.log(`Telegram webAppVersion telegramWA.user=${telegramWA.initDataUnsafe && telegramWA.initDataUnsafe.user}`)
+    console.log(`Telegram webAppVersion telegramWA.initParams=${telegramWA.initParams}`)
+    console.log(`Telegram webAppVersion telegramWA.initParams.tgWebAppVersion=${telegramWA.initParams && telegramWA.initParams.tgWebAppVersion}`)
+    console.log(`Telegram webAppVersion telegramWA.version=${telegramWA.version}`)
+  }
+
   setupNavigationClickListeners();
 
   document.getElementById("openSquadBtn").addEventListener("click", () => {
