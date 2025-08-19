d401fc6ceac617e66247462c9d41df0829e9f349
Kostya Zinoview <k.gig@list.ru>
2025-08-19 17:33:37 +0300

    add withdrawnotavailable bs on cashout page

--- cashout/cashout.js
+++ cashout/cashout.js
@@ -5,35 +5,75 @@ function openConfirmWithdrawlFlow() {
     const closeBtn = document.getElementById("withdrawClose")
     const withdrawBtn = document.getElementById("withdrawBtn")
 
-    function openWithdrawSheet() {
+    function open() {
         container.classList.add("active");
         withdrawSheet.classList.add("active");
         withdrawOverlay.classList.add("active");
     }
 
-    function closeWithdrawSheet() {
+    function close() {
         container.classList.remove("active");
         withdrawSheet.classList.remove("active");
         withdrawOverlay.classList.remove("active");
     }
 
-    openWithdrawSheet();
+    open();
 
     withdrawOverlay.addEventListener("click", () => {
-        closeWithdrawSheet();
+        close();
     })
 
     closeBtn.addEventListener("click", () => {
-        closeWithdrawSheet();
+        close();
     })
 
     withdrawBtn.addEventListener("click", () => {
-        closeWithdrawSheet();
+        close();
+    })
+}
+
+function openWithdrawlNotAvailableFlow() {
+    const container = document.getElementById("withdrawNotAvailableContainer");
+    const withdrawSheet = document.getElementById("withdrawNotAvailableSheet");
+    const withdrawOverlay = document.getElementById("withdrawNotAvailableOverlay");
+    const closeBtn = document.getElementById("withdrawNotAvailableClose")
+    const withdrawBtn = document.getElementById("withdrawNotAvailableBtn")
+    const levelText = document.getElementById("withdrawNotAvailableLevel")
+    const progress = document.getElementById("withdrawNotAvailableProgress");
+    
+    function open() {
+        container.classList.add("active");
+        withdrawSheet.classList.add("active");
+        withdrawOverlay.classList.add("active");
+        progress.style.width = `80%`
+        levelText.textContent = ` Level loyalty: 5`;
+    }
+
+    function close() {
+        container.classList.remove("active");
+        withdrawSheet.classList.remove("active");
+        withdrawOverlay.classList.remove("active");
+        progress.style.width = `0%`
+    }
+
+    open();
+
+    withdrawOverlay.addEventListener("click", () => {
+        close();
+    })
+
+    closeBtn.addEventListener("click", () => {
+        close();
+    })
+
+    withdrawBtn.addEventListener("click", () => {
+        close();
     })
 }
 
 window.onload = function () {
     document.getElementById("withdraw-tg-btn").addEventListener("click", () => {
-        openConfirmWithdrawlFlow()
+        openWithdrawlNotAvailableFlow()
     })
+
 }
